2373ada7b2190142c5f8e2673cae4bc8fecc930f
Harry Gogonis <harry@supplypike.com>
2021-10-19 19:58:45 +0000

    debuggigng

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import { HttpError } from '@kubernetes/client-node'
 import {PullRequestEvent} from '@octokit/webhooks-types' // eslint-disable-line import/no-unresolved
 import {fluxDeploy} from './deploy'
 
@@ -44,6 +45,9 @@ async function run(): Promise<void> {
       await deploy.rollout(deployImage)
     }
   } catch (error) {
+    if (error instanceof HttpError) {
+      core.info(`HttpError ${error.statusCode} :\n ${error.body}`)
+    }
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
