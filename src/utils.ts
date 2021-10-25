d4232d824c4a11680502bd4588fb6cda8cfee673
Harry Gogonis <harry@supplypike.com>
2021-10-25 19:43:38 +0000

    fix getInputRequired

--- src/utils.ts
+++ src/utils.ts
@@ -2,9 +2,13 @@ import * as core from '@actions/core'
 
 export function getInputRequired(key: string, defaultValue?: string): string {
   // getInput returns empty string if the value does not exist
-  const v: string = core.getInput(key) ?? defaultValue
+  const v = core.getInput(key)
   if (v !== '') {
     return v
+  }
+
+  if (defaultValue !== undefined) {
+    return defaultValue
   }
   throw new Error(`Missing required input ${key}`)
 }
