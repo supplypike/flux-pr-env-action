2d2702c3a534984410b03025692bf45049ba35f6
Harry Gogonis <harry@supplypike.com>
2021-11-01 14:31:16 +0000

    api tests

---
+++ __tests__/api.test.ts
@@ -0,0 +1,111 @@
+import {describe, expect, it, beforeEach} from '@jest/globals'
+import nock from 'nock'
+import {K8sApi} from '../src/api'
+
+import {mockGitRepo, mockKustomization} from './mocks'
+
+beforeEach(() => {
+  nock.disableNetConnect()
+})
+
+describe('#createNamespacedGitRepository', () => {
+  it('POST to gitrepositories API', async () => {
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .post(
+        '/apis/source.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/gitrepositories'
+      )
+      .reply(200)
+
+    await api.createNamespacedGitRepository('NAME', 'NAMESPACE', mockGitRepo)
+
+    expect(scope.done())
+  })
+})
+
+describe('#deleteNamespacedGitRepository', () => {
+  it('DELETE to gitrepositories API', async () => {
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .delete(
+        '/apis/source.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/gitrepositories/NAME'
+      )
+      .reply(200)
+
+    await api.deleteNamespacedGitRepository('NAME', 'NAMESPACE')
+
+    expect(scope.done())
+  })
+})
+
+describe('#createNamespacedKustomization', () => {
+  it('POST to kustomizations API', async () => {
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .post(
+        '/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/kustomizations'
+      )
+      .reply(200)
+
+    await api.createNamespacedKustomization(
+      'NAME',
+      'NAMESPACE',
+      mockKustomization
+    )
+
+    expect(scope.done())
+  })
+})
+
+describe('#getNamespacedKustomization', () => {
+  it('GET to kustomizations API', async () => {
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .get(
+        '/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/kustomizations/NAME'
+      )
+      .reply(200)
+
+    await api.getNamespacedKustomization('NAME', 'NAMESPACE')
+
+    expect(scope.done())
+  })
+})
+
+describe('#deleteNamespacedKustomization', () => {
+  it('DELETE to kustomizations API', async () => {
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .delete(
+        '/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/kustomizations/NAME'
+      )
+      .reply(200)
+
+    await api.deleteNamespacedKustomization('NAME', 'NAMESPACE')
+
+    expect(scope.done())
+  })
+})
+
+describe('#patchNamespacedKustomization', () => {
+  it('PATCH to kustomizations API', async () => {
+    const patch = [
+      {
+        op: 'replace',
+        path: '/spec/postBuild/substitute/image_tag',
+        value: 'latest'
+      }
+    ]
+    const api = K8sApi()
+    const scope = nock('http://localhost:8080')
+      .patch(
+        '/apis/kustomize.toolkit.fluxcd.io/v1beta1/namespaces/NAMESPACE/kustomizations/NAME',
+        patch
+      )
+      .reply(200)
+
+    await api.patchNamespacedKustomization('NAME', 'NAMESPACE', patch)
+
+    expect(scope.done())
+  })
+})
--- __tests__/deploy.test.ts
+++ __tests__/deploy.test.ts
@@ -1,49 +1,8 @@
 import {describe, expect, it, jest, beforeEach} from '@jest/globals'
-import {fluxDeploy, FluxDeployConfig} from '../src/deploy'
-import {GitRepositorySpec} from '../src/gitrepository'
-import {KustomizationSpec} from '../src/kustomization'
+import {fluxDeploy} from '../src/deploy'
+import {mockGitRepo, mockDeploy, mockKustomization} from './mocks'
 
 jest.mock('@actions/core')
-
-const mockDeploy: FluxDeployConfig = {
-  name: 'mock',
-  namespace: 'mock-ns',
-  kustomization: {
-    path: './kustomization/mock'
-  },
-  gitRepo: {
-    branch: 'main',
-    secretName: 'secret',
-    url: 'https://github.com/supplypike/flux-pr-env-action'
-  },
-  imageTag: 'latest'
-}
-
-const mockKustomization: KustomizationSpec = {
-  path: './kustomization/mock',
-  targetNamespace: 'mock-ns',
-  sourceRef: {
-    name: 'mock',
-    kind: 'GitRepository'
-  },
-  prune: true,
-  postBuild: {
-    substitute: {
-      image_tag: 'latest',
-      preview_name: 'mock'
-    }
-  },
-  interval: '1m0s'
-}
-
-const mockGitRepo: GitRepositorySpec = {
-  ref: {branch: 'main'},
-  url: 'https://github.com/supplypike/flux-pr-env-action',
-  secretRef: {
-    name: 'secret'
-  },
-  interval: '1m0s'
-}
 
 describe('#deploy', () => {
   let api: any
